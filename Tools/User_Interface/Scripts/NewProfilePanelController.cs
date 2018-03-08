264cc28ce763e2198c4c583b243898534b372ea8
Alexander Dudek <alexander.dudek@kuleuven.be>
2018-03-08 14:55:05 +0100

    namespace fix for newprofilepanelcontroller

--- Tools/User_Interface/Scripts/NewProfilePanelController.cs
+++ Tools/User_Interface/Scripts/NewProfilePanelController.cs
@@ -4,36 +4,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UI.Xml;
 using UnityEngine.UI;
+using Lars.Settings;
 
-class NewProfilePanelController : XmlLayoutController
+namespace Lars.UI
 {
-    public GameSettings settings;
 
-    public override void LayoutRebuilt(ParseXmlResult parseResult)
+    class NewProfilePanelController : XmlLayoutController
     {
-        // ParseXmlResult.Changed   => The Xml was parsed and the layout changed as a result
-        // ParseXmlResult.Unchanged => The Xml was unchanged, so no the layout remained unchanged
-        // ParseXmlResult.Failed    => The Xml failed validation
+        public GameSettings settings;
 
-        // Called whenever the XmlLayout finishes rebuilding the layout
-        // Use this function to make any dynamic changes (e.g. create dynamic lists, menus, etc.) or dynamically load values/selections for elements such as DropDown
+        public override void LayoutRebuilt(ParseXmlResult parseResult)
+        {
+            // ParseXmlResult.Changed   => The Xml was parsed and the layout changed as a result
+            // ParseXmlResult.Unchanged => The Xml was unchanged, so no the layout remained unchanged
+            // ParseXmlResult.Failed    => The Xml failed validation
+
+            // Called whenever the XmlLayout finishes rebuilding the layout
+            // Use this function to make any dynamic changes (e.g. create dynamic lists, menus, etc.) or dynamically load values/selections for elements such as DropDown
+        }
+
+        void addProfileBt()
+        {
+            //create new data object & add it to profiles
+            string nm = xmlLayout.GetElementById<InputField>("profileNameInput").text;
+            settings.AddNewProfile(nm);
+
+            //hide add panel
+            gameObject.SetActive(false);
+        }
+
+        void cancelAddProfileBt()
+        {
+            //hide add panel
+            gameObject.SetActive(false);
+        }
+
+
     }
-
-    void addProfileBt()
-    {
-        //create new data object & add it to profiles
-        string nm = xmlLayout.GetElementById<InputField>("profileNameInput").text;
-        settings.AddNewProfile(nm);
-
-        //hide add panel
-        gameObject.SetActive(false);
-    }
-
-    void cancelAddProfileBt()
-    {
-        //hide add panel
-        gameObject.SetActive(false);
-    }
-
 
 }
